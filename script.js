38a0812c2dfa682ec694ae5b63e1a1ed175e43bd
Jeff Ripke <jripke74@gmail.com>
2025-03-01 12:56:52 -0800

    add variable and console.log()

---
+++ script.js
@@ -0,0 +1,48 @@
+let adjective = "big";
+let adjective2 = "small";
+let noun = "house";
+let noun2 = "truck";
+let verb = "burning";
+let place = "spokane";
+const firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+adjective = "wide";
+adjective2 = "narrow";
+noun = "bush";
+noun2 = "tree";
+verb = "eating";
+place = "Post Falls";
+const secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("First story: " + firstStory);
+console.log("Second story: " + secondStory);
